4cdca9328a4e0728c84c96296ab7fcbc0e796c13
woojenoh <woojenoh@gmail.com>
2020-01-09 14:15:55 +0900

    InfiniteScroll - IO 사용 케이스를 분리

--- InfiniteScroll/src/AppWithIO.tsx
+++ InfiniteScroll/src/AppWithIO.tsx
@@ -1,6 +1,5 @@
 import React, { FC, useState, useEffect, useRef } from 'react';
 import axios from 'axios';
-import { debounce } from 'lodash';
 import ImageCard from './components/ImageCard';
 
 interface IImage {
@@ -14,12 +13,12 @@ interface IImage {
 const App: FC = () => {
   const [count, setCount] = useState(0);
   const [Images, setImages] = useState<IImage[]>([]);
+  const footerRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
-    window.addEventListener('scroll', scrollListener(setCount));
-
-    return () => {
-      window.removeEventListener('scroll', scrollListener(setCount));
+    if (footerRef.current) {
+      const io = ioGenerator(setCount);
+      io.observe(footerRef.current);
     }
   }, [])
 
@@ -35,14 +34,21 @@ const App: FC = () => {
   return (
     <div className="App">
       {Images && Images.map((d: IImage) => <ImageCard key={d.id} src={d.url} alt={d.title} />)}
+      <div ref={footerRef}>Footer</div>
     </div>
   );
 }
 
-const scrollListener = (setCount: React.Dispatch<React.SetStateAction<number>>) => debounce(() => {
-  if (document.documentElement.scrollTop + document.documentElement.clientHeight + 500 >= document.documentElement.scrollHeight) {
-    setCount((prev) => prev + 1);
-  }
-}, 200)
+const ioGenerator = (setCount: React.Dispatch<React.SetStateAction<number>>) => {
+  const io = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        setCount((prev) => prev + 1);
+      }
+    });
+  })
+
+  return io;
+}
 
 export default App;
