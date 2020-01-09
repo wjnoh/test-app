47ce26d703265e31ce191cc4f4cdf48224d5d5d4
woojenoh <woojenoh@gmail.com>
2020-01-09 13:45:25 +0900

    InfiniteScroll - IntersectionObserver 추가

--- InfiniteScroll/src/App.tsx
+++ InfiniteScroll/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useState, useEffect, } from 'react';
+import React, { FC, useState, useEffect, useRef, RefObject } from 'react';
 import axios from 'axios';
 import { debounce } from 'lodash';
 import ImageCard from './components/ImageCard';
@@ -13,30 +13,46 @@ interface IImage {
 
 const App: FC = () => {
   const [count, setCount] = useState(0);
-  const [data, setData] = useState<IImage[]>([]);
+  const [Images, setImages] = useState<IImage[]>([]);
+  // const footerRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     window.addEventListener('scroll', scrollListener(setCount));
     return () => {
       window.removeEventListener('scroll', scrollListener(setCount));
     }
+
+    // if (!footerRef.current) return;
+    // io.observe(footerRef.current);
   }, [])
 
   useEffect(() => {
-    axios(`https://jsonplaceholder.typicode.com/photos?_start=${count * 30}&_limit=30`)
-      .then(res => setData(data.concat(res.data)));
+    const fetchImages = (async () => {
+      const res = (await axios(`https://jsonplaceholder.typicode.com/photos?_start=${count * 20}&_limit=20`)).data as IImage[];
+      setImages((prev) => prev.concat(res));
+    })
+    fetchImages();
   }, [count])
+
+  // const io = new IntersectionObserver((entries) => {
+  //   entries.forEach(entry => {
+  //     if (entry.isIntersecting) {
+  //       setCount((prev) => prev + 1);
+  //     }
+  //   });
+  // })
 
   return (
     <div className="App">
-      {data && data.map((d: IImage) => <ImageCard key={d.id} src={d.url} alt={d.title} />)}
+      {Images && Images.map((d: IImage) => <ImageCard key={d.id} src={d.url} alt={d.title} />)}
+      {/* <div ref={footerRef}>Footer</div> */}
     </div>
   );
 }
 
 const scrollListener = (setCount: React.Dispatch<React.SetStateAction<number>>) => debounce(() => {
   if (document.documentElement.scrollTop + document.documentElement.clientHeight + 500 >= document.documentElement.scrollHeight) {
-    setCount((prev: number) => prev + 1);
+    setCount((prev) => prev + 1);
   }
 }, 200)
 
